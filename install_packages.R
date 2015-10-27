48d2be9cffdd4c14fea11259640ca87ce814c65a
Sulaiman Malik <smalik@plytos.com>
2015-10-27 13:13:36 -0400

    added cmd line arg

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,14 @@
-.libPaths(/<Data partition>/R/library)
-plist <- read.csv('~/Downloads/packages.csv')
-install.packages(as.character(plist[,1]))
+args=(commangArgs(TRUE))
+
+if(length(args) == 0) {
+    print('No path argument supplied.  Please provide the path for installing R packages')
+    print('E.G. <name of data partitio>/R/library')
+} else {
+    path = args[[i]]
+    print(path)
+    print('K\'plah!')
+}
+
+.libPaths(path)
+plist <- read.csv('https://github.com/smalik/R_package_list/blob/master/packages.csv', stringsAsFactors=FALSE, header=TRUE)
+install.packages(plist[,1])
